97198718a5bde63d105d41063c0eeba2fca7eba5
xihaotian <508396091@qq.com>
2022-09-23 15:42:50 +0800

    抠图完成

---
+++ src/com/bjsxt/util/GameUtil.java
@@ -0,0 +1,9 @@
+package com.bjsxt.util;
+
+import java.awt.*;
+
+public class GameUtil {
+    public static Image bg_img = Toolkit.getDefaultToolkit().createImage("imgs/背景草地.png");
+
+    public static Image xcqu_img =Toolkit.getDefaultToolkit().createImage("imgs/消除区域.png");
+}
